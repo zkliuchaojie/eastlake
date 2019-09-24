da3381df9e45abca1f85d34ef5e9a16aed50f8a7
liuchaojie <zkliuchaojie@163.com>
2019-09-24 10:26:57 +0800

    Reserve 32TB virtual address space for persistent object(PO) mapping, for more detail, please visit: http://note.youdao.com/noteshare?id=7579097ff30b58fa39b202ccd2ba31f5. Besides, we add a simple unit test, po_map_area_start_addr_ut, to check whether it comes into effect. Remember that, the unit test can only pass when the kernel in your computer uses this commited codes.

---
+++ include/linux/po_map.h
@@ -0,0 +1,34 @@
+/*
+ * Do po map.
+ * The name and position of this file are not good,
+ * and we should refector it in the future.
+ * We just support x86_64 architecture.
+ */
+
+#ifndef _LINUX_PO_MAP_H
+#define _LINUX_PO_MAP_H
+
+/*
+ * PAGE_SHIFT is defained in arch/x86/include/asm/page_types.h.
+ * Here, we redefine it, and this need to refactor.
+ */
+#define PAGE_SHIFT_REDEFINED	12
+
+/*
+ * DEFAULT_MAP_WINDOW is defained in arch/x86/include/asm/processor.h.
+ * Here, we redefine it, and this need to refactor.
+ */
+#define DEFAULT_MAP_WINDOW_REDEFINED	((1UL << 47) - (1UL << PAGE_SHIFT_REDEFINED))
+
+#define MAX_GAP			((0x3fffffUL << PAGE_SHIFT_REDEFINED) + \
+				(256UL << PAGE_SHIFT_REDEFINED))
+#define MAX_RANDOM		(((1UL << 32) - 1) << PAGE_SHIFT_REDEFINED)
+
+#define PO_MAP_AREA_SIZE	0x200000000000	// 32TB
+#define PO_MAP_AREA_START	(DEFAULT_MAP_WINDOW_REDEFINED - \
+				MAX_GAP - \
+				MAX_RANDOM - \
+				PO_MAP_AREA_SIZE)
+#define PO_MAP_AREA_END		(PO_MAP_AREA_START + PO_MAP_AREA_SIZE)
+
+#endif
