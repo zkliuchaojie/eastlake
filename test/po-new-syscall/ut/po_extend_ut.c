e62d6000b9310be3327cc22f34c2e168347bd7d4
liuchaojie <zkliuchaojie@163.com>
2019-12-18 10:45:14 +0800

    add unittest for po_extend

--- test/po-new-syscall/ut/po_extend_ut.c
+++ test/po-new-syscall/ut/po_extend_ut.c
@@ -91,3 +91,57 @@ TEST(po_extend, exceed_MAX_BUDDY_ALLOC_SIZE)
 	ASSERT_EQ(retval1, 0);
 
 }
+
+TEST(po_extend, not_aligned_with_MAX_BUDDY_ALLOC_SIZE)
+{
+	char poname[] = "f";
+	int pod1;
+	long long retval1;
+	char *c1, *c2;
+
+	pod1 = syscall(400, poname);
+	ASSERT_GE(pod1, 0);
+
+	c1 = (char *)syscall(406, pod1, 2*MAX_BUDDY_ALLOC_SIZE + PAGE_SIZE_REDEFINED, \
+		PROT_READ | PROT_WRITE, MAP_PRIVATE);
+	ASSERT_GE((unsigned long)c1, 0);
+	c1[0] = 'a';
+	//printf("%c\n", c[0]);
+
+	retval1 = syscall(403, pod1, 0);
+	ASSERT_GE(retval1, 0);
+
+	retval1 = syscall(401, poname, 0);
+	ASSERT_EQ(retval1, 0);
+
+}
+
+TEST(po_extend, extend_twice_both_bigger_than_MAX_BUDDY_ALLOC_SIZE)
+{
+	char poname[] = "f";
+	int pod1;
+	long long retval1;
+	char *c1, *c2;
+
+	pod1 = syscall(400, poname);
+	ASSERT_GE(pod1, 0);
+
+	c1 = (char *)syscall(406, pod1, 2*MAX_BUDDY_ALLOC_SIZE, \
+		 PROT_READ | PROT_WRITE, MAP_PRIVATE);
+	ASSERT_GE((unsigned long)c1, 0);
+	c1[0] = 'a';
+	//printf("%c\n", c[0]);
+
+	c2 = (char *)syscall(406, pod1, 2*MAX_BUDDY_ALLOC_SIZE,  \
+		PROT_READ | PROT_WRITE, MAP_PRIVATE);
+	ASSERT_GE((unsigned long)c2, 0);
+	c2[0] = 'b';
+	//printf("%c\n", c[0]);
+	ASSERT_EQ(c1[0], 'a');
+
+	retval1 = syscall(403, pod1, 0);
+	ASSERT_GE(retval1, 0);
+
+	retval1 = syscall(401, poname, 0);
+	ASSERT_EQ(retval1, 0);
+}
