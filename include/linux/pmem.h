85bf4ac1c0bbc25e6f197a79220f1bbc80d71245
liuchaojie <zkliuchaojie@163.com>
2020-09-24 11:25:55 +0800

    move functions in pmem.h to pmem.c

--- include/linux/pmem.h
+++ include/linux/pmem.h
@@ -2,11 +2,6 @@
 #define _LINUX_PMEM_H
 
 #include <linux/types.h>
-#include <linux/mmzone.h>
-#include <linux/nodemask.h>
-#include <linux/gfp.h>
-#include <linux/memory_hotplug.h>
-#include <asm/sparsemem.h>
 
 #ifdef CONFIG_ZONE_PM_EMU
 
@@ -15,47 +10,8 @@ struct pmeminfo {
 	__kernel_ulong_t freepmem;   /* Available persistent memory */
 };
 
-static inline unsigned long global_pm_zone_free_pages(void)
-{
-	unsigned long free_pages = 0;
-	int nid, i;
-
-	for_each_online_node(nid) {
-		pg_data_t *pgdat = NODE_DATA(nid);
-		if (pgdat->nr_pm_zones != 0) {
-			for(i = 0; i < MAX_NR_PM_ZONES; i++) {
-				struct pm_zone *zone = &pgdat->node_pm_zones[i];
-				struct pm_super *super = zone->super;
-				free_pages += super->free;
-			}
-		}
-	}
-	return free_pages;
-}
-
-static inline void extend_memory_with_pmem(void)
-{
-	struct pt_page *page;
-	unsigned long long start, size;
-	int result;
-
-	pr_info("MAX_ORDER - 1: %d", MAX_ORDER - 1);
-	pr_info("SECTION_SIZE_BITS: %d", SECTION_SIZE_BITS);
-
-
-	page = alloc_pt_pages_node(0, GPFP_KERNEL, MAX_ORDER - 1);
-	if (page != NULL) {
-		start = (pt_page_to_pfn(page)<<12);
-		size = (1UL << (MAX_ORDER - 1 + 12));
-		pr_info("extend memory with pmem: [mem %#018llx-%#018llx]\n", \
-			   start, start + size -1);
-		/*
-		result = add_memory(0, start, size);
-		if (result < 0)
-			pr_info("extend memory with pmem failed");
-		*/
-	}
-}
+extern unsigned long global_pm_zone_free_pages(void);
+extern void extend_memory_with_pmem(void);
 
 #endif
 
--- mm/pmem.c
+++ mm/pmem.c
@@ -4,12 +4,64 @@
 
 #include <linux/pmem.h>
 #include <linux/syscalls.h>
+#include <linux/mmzone.h>
+#include <linux/nodemask.h>
+#include <linux/gfp.h>
+#include <linux/memory_hotplug.h>
+#include <asm/sparsemem.h>
 
 /* this syscall is used to debug persistent memory management */
 SYSCALL_DEFINE1(debugger, unsigned int, op)
 {
 	if (op == 0)
 		return 0;
+	if (op == 1) {
+		extend_memory_with_pmem();
+	}
+
 	return op;
+}
+
+inline unsigned long global_pm_zone_free_pages(void)
+{
+	unsigned long free_pages = 0;
+	int nid, i;
+
+	for_each_online_node(nid) {
+		pg_data_t *pgdat = NODE_DATA(nid);
+		if (pgdat->nr_pm_zones != 0) {
+			for(i = 0; i < MAX_NR_PM_ZONES; i++) {
+				struct pm_zone *zone = &pgdat->node_pm_zones[i];
+				struct pm_super *super = zone->super;
+				free_pages += super->free;
+			}
+		}
+	}
+	return free_pages;
+}
+
+inline void extend_memory_with_pmem(void)
+{
+	struct pt_page *page;
+	unsigned long long start, size;
+	int result;
+
+	pr_info("MAX_ORDER - 1: %d", MAX_ORDER - 1);
+	pr_info("SECTION_SIZE_BITS: %d", SECTION_SIZE_BITS);
+
+
+	page = alloc_pt_pages_node(0, GPFP_KERNEL, MAX_ORDER - 1);
+	if (page != NULL) {
+		start = (pt_page_to_pfn(page)<<12);
+		size = (1UL << (MAX_ORDER - 1 + 12));
+		pr_info("extend memory with pmem: [mem %#018llx-%#018llx]\n", \
+			   start, start + size -1);
+
+		result = add_memory(0, start, size);
+		if (result < 0)
+			pr_info("extend memory with pmem failed");
+		else
+			pr_info("extend memory success, result: %d", result);
+	}
 }
 
